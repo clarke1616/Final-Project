ef587b772d7e5ebeec3ff6e1739d174dff4d47e3
clarke1616 <117293712+clarke1616@users.noreply.github.com>
2022-11-04 13:06:09 -0400

    Add files via upload

---
+++ preposal .md.py
@@ -0,0 +1,18 @@
+# Preproposal
+#Emily Clark
+
+## What idea(s) do you have for your final project?
+
+create a program that can help you do your taxes - like a personal acountant since I am an accounting major
+or a web page that helps you create a budget
+playing a card game like go fish
+
+
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+Just me 
+
+## Do you have any questions of your own?
+
+no
