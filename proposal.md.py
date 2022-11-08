2cba365ee41169ebec0c332a6d34b2716c18f59b
clarke1616 <117293712+clarke1616@users.noreply.github.com>
2022-11-08 14:19:46 -0500

    Add files via upload

---
+++ proposal.md.py
@@ -0,0 +1,47 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+Personal Accoutant 
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+A website that can hep you calculate your taxes and assist you in fillinf out tax returns. 
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+The software will take input from the user on information regarding income, expenses, and other details that can effect the taxes of the taxpayer.
+It will use the the information to deduct specific eleigable deductions, calculate payments based on class & status to assistwith filling out
+their 1040 tax form.
+The end goal would be to be able to print what each line item will be in their personal 1040 so the taxpayer can easily input the numbers into their 1040
+and complete their tax return with the assistance of the accountant. 
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+TODO, if applicable
+N/A
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+TODO, if applicable
+N/A
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+I will be able to define the income tax class the user would be in and define their Taxable income, their tax due, and their tax rate. 
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+I will be able to state deductions and exceptions for the specific user
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+I will be able to print out line items 1 - 33 on the 1040 tax return
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+I will first make an outline of the tax structor and form a lost of input i will need to ask the user. I will then need to make functions that can
+calculate different deductions and tax rates. After I have the functions, I will need to program them so they are called when they are relevent to the
+user. I will use the IRS guide to filling out a 1040 to help guide me in the information needed to calculate the end result.
