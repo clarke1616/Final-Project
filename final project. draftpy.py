23af975c13656b031dad858fb9a2788ade3bbf4b
clarke1616 <117293712+clarke1616@users.noreply.github.com>
2022-11-13 13:34:14 -0500

    Add files via upload

---
+++ final project. draftpy.py
@@ -0,0 +1,57 @@
+###form 1040, 2022 Tax Year
+
+#print ("By running this program, you will be asked a series a questions that will be used to calculate the line items on your form 1040.")
+#print ("You will need access to your W-2 form as well as your 1099 -IOD form if you have recieved interest this tax year")
+
+##Line 1 - Wages Salaries and Tips Etc
+def wagesAndSalaries():
+     line1 = int(input ("What was your total wages, salaries and tips for 2022 - this can be found in BOX 1 of your W-2."))
+     return line1
+
+
+## Line 2a - Tax exempt Interest
+
+def exemptInterest ():
+    ei = input("Did you recieve any interest in 2022 from municiple (state) bonds? Y or N")
+    if ei == 'Y' or ei == 'y':
+        oidBox2 = int(input ("What is box 2 on your 1099-OID?"))
+        oidBox11 = int(input ("What is box 11 on your 1099-OID?"))
+        line2a =  oidBox2 + oidBox11
+        return line2a
+    else:
+        line2a = 0
+        return line2a
+##figure out how to get them all to print at the bottom
+#print ('Line 1 =' ,wagesAndSalaries())
+#print ('Line 2a = ', exemptInterest())
+
+##Line 2b - taxable interest
+def taxableInterest ():
+    ti = input("Did you recieve any interest in 2022 excluing interest from municiple (state) bonds? Y or N")
+    if ti == 'Y' or ti == 'y':
+        line2b = int(input('What is the total taxable interest income found on your 1099-IOD?' ))
+        #if line2b > 1500:
+           # messege = str('*Please fill out and attach Schedual B to the back of your 1040. This can be found at https://www.irs.gov/pub/irs-pdf/f1040sb.pdf')
+            #print (messege)
+        return line2b
+    else:
+        line2b = 0
+        return line2b
+#print ('Line 2b =',taxableInterest())
+
+
+
+
+
+
+                           
+                            
+                            
+
+        
+                    
+        
+                       
+                      
+                     
+    
