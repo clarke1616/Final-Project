293c7ba8cd4ccc7b0c2fd7ec94e5d2a7dba15396
clarke1616 <117293712+clarke1616@users.noreply.github.com>
2022-11-28 11:09:03 -0500

    Add files via upload

---
+++ status.md.py
@@ -0,0 +1,34 @@
+# Status Report
+
+#### Your name
+
+Emily Clark
+
+#### Your section leader's name
+
+Zhengkang Fan
+
+#### Project title
+
+Personal Accountant 
+
+***
+
+Short answers for the below questions suffice. If you want to alter your plan for your project (and obtain approval for the same), be sure to email your section leader directly!
+
+#### What have you done for your project so far?
+
+I have some of the calculations for the line items on the 1040 coded. I used functions
+for each line item and my plan is to have them all return in order at the end so
+the user can eaisly copy the numbers onto their 1040.
+
+#### What have you not done for your project yet?
+
+I have not finished all the line items and I also have not tried to import the 1040
+as a file. Depending on my progress, I may try and import the 1040 and have the
+line items print in the section they go with.
+
+#### What problems, if any, have you encountered?
+
+I have been having trouble figuring out how to get all the Functions to return the
+numbers at the end rather than after each function. 
