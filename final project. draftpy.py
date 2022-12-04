1acffeb9ab6a003035abab1544495a5d7845ef35
clarke1616 <117293712+clarke1616@users.noreply.github.com>
2022-12-04 13:21:41 -0500

    Update final project. draftpy.py

--- final project. draftpy.py
+++ final project. draftpy.py
@@ -1,5 +1,11 @@
 ###form 1040, 2022 Tax Year
+
 '''
+print('Welcome! The personal account is ready to do your taxes!')
+
+
+
+
 print ("By running this program, you will be asked a series a questions that will be used to calculate the line items on your form 1040.")
 print ("In order to complete this document, you will need to have the following forms:")
 print ('            - W-2')
@@ -7,10 +13,11 @@ print ('            - 1099- IOD (if applicable)')
 print ('            - 1099- DIV')
 print ('            - 1099- R')
 print ('            - SSA- 1099')
+print ('            - Schedual 1')
 print ('            - Schedual D (if applicable)')
 print ('')
 print('')
-
+'''
 #############################################################################################################################################
 
 ##Inputs needed
@@ -34,7 +41,7 @@ if c == 'Y' or c == 'y':
         #return line2b
 else:
      x = 0
-]     
+     
 ##For lines 3a and 3 b
 qd = int(input ("Looking at line 1b on your 1099-DIV, please enter the number given"))
 od = int(input ("Looking at line 1a on your 1099-DIV, please enter the number given"))
@@ -45,6 +52,7 @@ if r == 'y' or r == 'Y':
      amount = int(input('How much is being rolled over?'))
 else:
      amount = 0
+
 IRATax = int(input('Looking at your 1099-R, what is the amount in box 1?'))
 
 
@@ -55,9 +63,27 @@ taxableAnnuities = int(input('Looking at your form 1099-R, what amount of your P
 #Lines 6a & 6b
 socsecTotal = int(input('Looking at your SSA-1099, what is the amount in box 3?'))
 socsecPaid = int(input('Looking at your SSA-1099, what is the amount in box 4?'))               
-'''
+
 ##Line 7
 
+capital = input('Did you sell any capital assets? (Y or N)')
+if capital == 'y' or capital == 'Y':
+     capamount = int(input('What is your capital gain or loss?'))
+else:
+     capamount = 0
+
+##line 8
+
+other = int(input('Looking at Schedule 1, what is the amount on line 10?'))
+
+#line 10
+adj = int(input('Looking at Schedule 1, what is line 26?'))
+
+#Line 12a
+deduction = input('are you filing as: A - Single B - Married Filing seperatly C- Married filing jointly D- Qualified Widow, or E - Head of household?')
+
+##Line 12b
+don = int(input('How much did you donate to charity this year? **MAX $300 for single and $600 joint**'))
 
 ################################################################################################################################################
 
@@ -135,27 +161,81 @@ def socialsecuritybenefits():
 def taxedsocialsecurity():
      taxedss = socsecPaid
      return taxedss
-          
+##Line 7
+def capitalgl():
+     capgainloss = capamount
+     return capgainloss
+##line 8
+def otherincome():
+     otherinc = other
+     return otherinc
+##line 9
+
+def totalincome():
+     tot = wagesAndSalaries() + taxableInterest() + ordinaryDiv()+ IRADistTaxableAmount()+ taxablePensionsAndAnnuities()+ taxedsocialsecurity()+ capitalgl()+ otherincome()
+     return tot
+
+
+##Line 10
+def adjToIncome():
+     adjustments = adj
+     return adjustments
+
+#Line 11
+def AGI ():
+     agi = totalincome() - adjToIncome()
+     return agi
      
-'''                   
+##Line 12a
+def deductions():
+     deductionAmount = 0
+     deduct = deduction
+     if deduct == 'A' or deduct == 'a' or deduct == 'B' or deduct == 'b':
+          deductionAmount += 12550
+     elif deduct == 'C' or deduct == 'c' or deduct == 'D' or deduct == 'd':
+          deductionAmount += 25100
+     elif deduct == 'E' or deduct == 'e':
+          deductionAmount += 18800
+     return deductionAmount
+
+##Line 12b
+def twelveb():
+     d = don
+     return d
+##Line 12c
+def twelveC():
+     twelveTotal = twelveb()+ deductions()
+     return twelveTotal
+
+
+################################################################################################################################################################
 print('')
 print('')
 print('')
+print('')
+print('')
+print('')
+
 print('Your 1040 Tax form should be filled out as follows:')
+print('')
+print('')
+print('')
+
 print ('')
-print ('1. Wages, salaries, tips etc. Attach From(s) W-1 . . . . . . . . . . . . . . . . ' ,wagesAndSalaries())
+print ('1. Wages, salaries, tips etc. Attach From(s)  W-1 . . . . . . . . . . . . . . . . ' ,wagesAndSalaries())
 print ('2a. Tax-exempt interest . . . 2a  ', exemptInterest(), '      b  Taxable Interest  . . . . . . . . 2b', taxableInterest())
 print ('3a. Qualified dividends . . . 3a  ', qualifiedDividends(), '      b  Ordinary dividends. . . . . . . . 3b',ordinaryDiv())
 print ('4a. IRA Distributions. . . . .4a  ', IRADistributions(),'      b  Taxable amount  . . . . . . . . . 4b', IRADistTaxableAmount())
 print ('5a. Pensions and annuities . .5a  ', pensionsAndAnnunities(),'      b  Taxable amount  . . . . . . . . . 5b', taxablePensionsAndAnnuities())
-print ('6a. Social security benefits .6a  ', socialsecuritybenefits(), '      b  Taxable amount . . . . . . . . . .6b', taxedsocialsecurity())                    
-'''                            
-                            
-
-        
-                    
-        
-                       
+print ('6a. Social security benefits .6a  ', socialsecuritybenefits(), '      b  Taxable amount . . . . . . . . . .6b', taxedsocialsecurity())
+print ('7 Capital gain or (loss). Attach Schedule D is required. If not, check here . . 7 ', capitalgl())
+print ('8 Other income from Schedule 1, line 10 . . . . . . . . . . . . . . . . . . . . 8 ',otherincome())                           
+print ('9 Add lines 1, 2b, 3b, 4b, 5b, 6b, 7 and 8. This is your total income . . . . . 9 ', totalincome())                            
+print ('10 Adjustments to income from Schedule 1, line 26 . . . . . . . . . . . . . . .10 ', adjToIncome())
+print ('11 Subtract line 10 from line 9. This is your adjusted gross income . . . . . .11 ', AGI())
+print ('12a Standard Deduction or itemized deductions (from schedule A) . . 12a', deductions())
+print ('  b Charitable contributions id you take the standard deduction . . 12b',twelveb())     
+print ('  c Add lines 12a and 12b . . . . . . . . . . . . . . . . . . . . . . . . . . .12c',twelveC())                     
                       
                      
     
